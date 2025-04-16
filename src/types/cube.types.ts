9be737f322f18a64a5b3254067f2ab1b7a858759
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-04-17 00:14:36 +0900

    toDisplayData

---
+++ src/types/cube.types.ts
@@ -0,0 +1,10 @@
+export type Cube = {
+  center: {
+    latitude_deg: number;
+    longitude_deg: number;
+    altitude_m: number;
+  };
+  height_m: number;
+  width_m: number;
+  depth_m: number;
+};
--- src/utils/to-display-data.ts
+++ src/utils/to-display-data.ts
@@ -1,3 +1,25 @@
-export default function toDisplayData() {
-  //ボクセルのPureIDから
+import { PureVoxel } from "../types/pureVoxel.types";
+import { Cube } from "../types/cube.types";
+
+export default function toDisplayData(pureVoxels: PureVoxel[]): Cube[] {
+  //ボクセルのPureIDから中心点、縦、横、奥行きの長さを作成する
+  let result: Cube[] = [];
+  for (let i = 0; i < pureVoxels.length; i++) {
+    result.push({
+      center: {
+        latitude_deg: (90 / pureVoxels[i].Z ** 2 + 1) * pureVoxels[i].Y,
+        longitude_deg: (180 / pureVoxels[i].Z ** 2 + 1) * pureVoxels[i].X,
+        altitude_m:
+          ((33554432 * 2) / pureVoxels[i].Z ** 2 + 1) * pureVoxels[i].F -
+            (33554432 / pureVoxels[i].Z ** 2) * pureVoxels[i].Z <
+          0
+            ? -1
+            : 1,
+      },
+      height_m: 33554432 / pureVoxels[i].Z ** 2,
+      width_m: 40075016.68 / pureVoxels[i].Z ** 2,
+      depth_m: 40075016.68 / pureVoxels[i].Z ** 2,
+    });
+  }
+  return result;
 }
