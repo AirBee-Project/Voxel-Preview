e9d733cbb475476cf4ffd7ab21399878ca3add3f
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-04-29 11:29:18 +0900

    pointの表示に成功

---
+++ src/utils/colorHexToRgba.ts
@@ -0,0 +1,22 @@
+export default function colorHexToRgba(
+  hex: string,
+  opacity: number
+): [number, number, number, number] {
+  // まず # を除去
+  const cleanHex = hex.replace("#", "");
+
+  // 6桁以外はエラーにする
+  if (cleanHex.length !== 6) {
+    throw new Error("Invalid HEX color");
+  }
+
+  // R, G, Bをそれぞれ取り出して10進数に変換
+  const r = parseInt(cleanHex.substring(0, 2), 16);
+  const g = parseInt(cleanHex.substring(2, 4), 16);
+  const b = parseInt(cleanHex.substring(4, 6), 16);
+
+  // opacity (0〜100) を 0〜255 に変換
+  const a = Math.round((opacity / 100) * 255);
+
+  return [r, g, b, a];
+}
--- src/utils/generateLayer.ts
+++ src/utils/generateLayer.ts
@@ -2,19 +2,34 @@ import { Layer, LayersList } from "deck.gl";
 import { TileLayer } from "@deck.gl/geo-layers";
 import { BitmapLayer, GeoJsonLayer } from "@deck.gl/layers";
 import { Item } from "../types/Item";
+import { GeoJSON } from "geojson";
+import colorHexToRgba from "../utils/colorHexToRgba";
 /**
  * StateであるItem[]を入れると、DeckglのLayerに変換し出力する関数
  */
 export default function generateLayer(item: Item[]): LayersList {
-  let displayItem: Item[] = item.filter((e) => !e.isDeleted && !e.isVisible);
+  let pointItem: Item<"point">[] = item.filter(
+    (e): e is Item<"point"> =>
+      !e.isDeleted && !e.isVisible && e.type === "point"
+  );
 
-  let reuslt: LayersList = [];
-
-  const geoJsonLayer = new GeoJsonLayer({
+  //PointはまとめてGeoJsonLayerとして表示
+  const pointGeoJsonLayer = new GeoJsonLayer({
     id: "GeoJsonLayer",
-    data:
+    data: generatePointGeoJson(pointItem),
+    pickable: true,
+    filled: true,
+    pointRadiusMinPixels: 1,
+    pointRadiusScale: 1,
+    getFillColor: (d) => d.properties.color, // 個別カラー
+    getRadius: (d) => d.properties.radius, // 個別サイズ
   });
 
+  //LineはまとめてGeoJsonLayerとして表示
+
+  //VoxelはPolygonLayerとしてまとめて出力
+
+  //国土地理院から取得したTileMapを表示
   const tileMapLayer = new TileLayer({
     id: "TileMapLayer",
     data: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
@@ -35,6 +50,30 @@ export default function generateLayer(item: Item[]): LayersList {
     },
     pickable: true,
   });
-  reuslt.push(tileMapLayer);
+
+  let reuslt: LayersList = [tileMapLayer, pointGeoJsonLayer];
   return reuslt;
+}
+
+function generatePointGeoJson(point: Item<"point">[]): GeoJSON {
+  const result: GeoJSON = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  for (let i = 0; i < point.length; i++) {
+    result.features.push({
+      type: "Feature",
+      properties: {
+        color: colorHexToRgba(point[i].data.color, point[i].data.opacity),
+        radius: point[i].data.size,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [point[i].data.lon, point[i].data.lat], // 東京
+      },
+    });
+  }
+
+  return result;
 }
